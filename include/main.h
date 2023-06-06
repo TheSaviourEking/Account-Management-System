b51deb1b236f7aa6ac156582495154ac50ddb0cb
TheSaviourEking <saviour.eking@outlook.com>
2023-06-06 03:52:35 +0100

    chore: adds includes directory for header

---
+++ include/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* HEADER FILES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* FUNCTIONS */
+void close(void);
+void menu();
+float interest(float t,float amount,int rate);
+void fordelay(int j);
+void new_acc();
+void view_list();
+void edit(void);
+void transact(void);
+void erase(void);
+void see(void);
+void close(void);
+void menu(void);
+
+
+#endif /* MAIN_H */
